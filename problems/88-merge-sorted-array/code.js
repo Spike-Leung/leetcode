4ab4d09881965ae8b67f7772844264265ddc7939
Spike <l-yanlei@hotmail.com>
2021-08-08 20:46:23 +0800

    feat: space opt for 88-merge-sorted-array

--- problems/88-merge-sorted-array/code.js
+++ problems/88-merge-sorted-array/code.js
@@ -11,24 +11,20 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
-  const sortedArr = [];
-  let p1 = 0;
-  let p2 = 0;
+  const length = m + n;
+  let p1 = m - 1;
+  let p2 = n - 1;
 
-  while (p1 < m || p2 < n) {
-    if (p1 === m) {
-      sortedArr.push(nums2[p2++]);
-    } else if (p2 === n) {
-      sortedArr.push(nums1[p1++]);
-    } else if (nums1[p1] >= nums2[p2]) {
-      sortedArr.push(nums2[p2++]);
+  for (let i = length - 1; i >= 0; i--) {
+    if (p1 < 0) {
+      nums1[i] = nums2[p2--];
+    } else if (p2 < 0) {
+      nums1[i] = nums1[p1--];
+    } else if (nums2[p2] > nums1[p1]) {
+      nums1[i] = nums2[p2--];
     } else {
-      sortedArr.push(nums1[p1++]);
+      nums1[i] = nums1[p1--];
     }
-  }
-
-  for (let i = 0; i < m + n; i++) {
-    nums1[i] = sortedArr[i];
   }
 };
 
