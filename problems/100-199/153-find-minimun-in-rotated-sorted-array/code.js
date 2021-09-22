dc728b7c9aec6a03e8b4af0849fdffec05cbd578
Spike <l-yanlei@hotmail.com>
2021-09-22 18:09:22 +0800

    feat: method 2 of 153

--- problems/100-199/153-find-minimun-in-rotated-sorted-array/code.js
+++ problems/100-199/153-find-minimun-in-rotated-sorted-array/code.js
@@ -3,31 +3,40 @@
  *
  * https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/
  *
+ * After rotated, left most value is always greater than right most value
+ * find the minimun "right most value"
+ *
+ * https://www.geekxh.com/1.9.%E4%BA%8C%E5%88%86%E6%B3%95%E7%B3%BB%E5%88%97/904.html#_01%E3%80%81%E9%A2%98%E7%9B%AE%E7%A4%BA%E4%BE%8B
+ *
  * @param {number[]} nums
  * @return {number}
  */
 var findMin = function (nums) {
   let left = 0;
-  let right = nums.length - 1;
-  let min;
+  let right = nums.length;
 
-  while (left <= right) {
+  if (nums[left] < nums[right - 1]) {
+    return nums[left];
+  }
+
+  while (left < right) {
     const mid = Math.floor((right - left) / 2) + left;
     const midVal = nums[mid];
-    // 左边有序
-    if (nums[left] <= midVal) {
-      min =
-        min !== undefined
-          ? Math.min(nums[left], min)
-          : Math.min(nums[left], midVal);
+
+    if (nums[0] === midVal) {
       left = mid + 1;
-    } else {
-      min = min !== undefined ? Math.min(midVal, min) : midVal;
-      right = mid - 1;
+    } else if (nums[0] < midVal) {
+      left = mid + 1;
+    } else if (nums[0] > midVal) {
+      right = mid;
     }
   }
 
-  return min;
+  if (left === nums.length) {
+    return nums[left - 1];
+  }
+
+  return nums[left];
 };
 
 module.exports = findMin;
