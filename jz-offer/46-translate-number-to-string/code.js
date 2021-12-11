7cc8b3db1cf44afd5ec56acb3f25cb8e3333a0e0
Spike Leung <l-yanlei@hotmail.com>
2021-12-12 01:01:21 +0800

    feat: 45-translate DP

--- jz-offer/46-translate-number-to-string/code.js
+++ jz-offer/46-translate-number-to-string/code.js
@@ -4,31 +4,32 @@
  * @return {number}
  */
 var translateNum = function(num) {
-  if (num < 0) {
-    return 0;
-  }
+  const dp = [];
+  dp[0] = 1; // dp[2] = dp[1] + dp[0] = 2 => dp[0] = 1
+  dp[1] = 1; // 只有一个数字的时候
 
   const strNum = "" + num;
+  const length = strNum.length;
 
-  if (strNum.length === 0) {
-    return 0;
+  // 从第二个数字开始遍历
+  for (let i = 1; i < length; i++) {
+    const siblingNumber = +`${strNum[i - 1]}${strNum[i]}`;
+    // i 是数字的下标，第几个数字是 i + 1
+    const count = i + 1;
+
+    dp[count] = 0;
+
+    // 如果可以以两个字符翻译，则统计一个字符和两个字符的翻译数量
+    if (siblingNumber >= 10 && siblingNumber <= 25) {
+      dp[count] += (dp[count - 1] + dp[count - 2]);
+    } else {
+      // 否则只统计以一个字符翻译的数量
+      dp[count] += dp[count - 1];
+    }
   }
 
-  if (strNum.length === 1) {
-    return 1;
-  }
-
-  const translateSingleNumCount = translateNum(+strNum.slice(1));
-  let translateTwoNumCount = 0;
-
-  const twoNum = +strNum.slice(0, 2);
-
-  if (twoNum >= 10 && twoNum <= 25) {
-    translateTwoNumCount = translateNum(+strNum.slice(2));
-  }
-
-
-  return translateSingleNumCount + translateTwoNumCount;
+  // 返回最后一个字符的数量
+  return dp[length];
 };
 
 module.exports = translateNum;
