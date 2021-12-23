dc7de01094befa0b6910780ac15ed191f0cbf8cc
Spike Leung <l-yanlei@hotmail.com>
2021-12-23 15:10:22 +0800

    feat: 5-longest-palindromic-substring (DP)

---
+++ problems/0-99/5-longest-palindromic-substring/code.js
@@ -0,0 +1,48 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+  if (s.length < 2) {
+    return s;
+  }
+
+  let length = 1;
+  let begin = 0;
+  const columns = Array.from({ length: s.length }).fill(false);
+  let dp = Array.from({ length: s.length }).fill(columns);
+
+  // 初始化对角线，i = i , 只有一个元素时，肯定是回文
+  for (let i = 0; i < s.length; i++) {
+    dp[i][i] = true;
+  }
+
+  // 先从某一列开始
+  for (let j = 1; j < s.length; j++) {
+    // 遍历每一行的值，i < j，范围才成立，不需要遍历到 s.length
+    for (let i = 0; i < j; i++) {
+      // 如果左右边界的值不同，不可能形成回文
+      if (s[i] !== s[j]) {
+        dp[i][j] = false;
+      } else {
+        // 减去两个数字后，就剩下一个，一个也能构成回文，所以为 true
+        if (j - i < 3) {
+          dp[i][j] = true;
+        } else {
+          // 判断去掉两头后，剩下是否构成回文
+          dp[i][j] = dp[i + 1][j - 1];
+        }
+      }
+
+      // 当构成回文时，更新 begin 和 长度
+      if (dp[i][j] && ((j - i + 1) > length)) {
+        length = j - i + 1;
+        begin = i;
+      }
+    }
+  }
+
+  return s.slice(begin, begin + length);
+};
+
+module.exports = longestPalindrome;
---
+++ problems/0-99/5-longest-palindromic-substring/code.test.js
@@ -0,0 +1,32 @@
+const longestPalindrome = require("./code");
+
+const str = "babad";
+const subStr = "bab";
+test(`Longest palindrome substring of ${str} is ${subStr}`, () => {
+  const expected = ['bab', 'aba'].includes(longestPalindrome(str));
+  expect(expected).toBe(true);
+});
+
+const str1 = "cbbd";
+const subStr1 = "bb";
+test(`Longest palindrome substring of ${str1} is ${subStr1}`, () => {
+  expect(longestPalindrome(str1)).toBe(subStr1);
+});
+
+const str2 = "a";
+const subStr2 = "a";
+test(`Longest palindrome substring of ${str2} is ${subStr2}`, () => {
+  expect(longestPalindrome(str2)).toBe(subStr2);
+});
+
+const str3 = "ac";
+const subStr3 = "a";
+test(`Longest palindrome substring of ${str3} is ${subStr3}`, () => {
+  expect(longestPalindrome(str3)).toBe(subStr3);
+});
+
+const str4 = "acca";
+const subStr4 = "acca";
+test(`Longest palindrome substring of ${str4} is ${subStr4}`, () => {
+  expect(longestPalindrome(str4)).toBe(subStr4);
+});
