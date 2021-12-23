ff581801737a740128fc604fefca3c515602f7b8
Spike Leung <l-yanlei@hotmail.com>
2021-12-23 16:21:23 +0800

    feat: 5-longest-palindromic-substring (double-pointer)

--- problems/0-99/5-longest-palindromic-substring/code.js
+++ problems/0-99/5-longest-palindromic-substring/code.js
@@ -44,4 +44,30 @@ var longestPalindrome = function(s) {
   return s.slice(begin, begin + length);
 };
 
-module.exports = longestPalindrome;
+var longestPalindromeWithDoublePointer = function(s) {
+  let res = "";
+
+  for (let i = 0; i < s.length; i++) {
+    const s1 = palindrome(s, i, i);
+    const s2 = palindrome(s, i, i + 1);
+
+    res = res.length > s1.length ? res : s1;
+    res = res.length > s2.length ? res : s2;
+  }
+
+  return res;
+};
+
+function palindrome(s, l, r) {
+  while (l >= 0 && r < s.length && s[l] === s[r]) {
+    l--;
+    r++;
+  }
+
+  return s.slice(l + 1, r);
+}
+
+module.exports = {
+  longestPalindrome,
+  longestPalindromeWithDoublePointer
+};
--- problems/0-99/5-longest-palindromic-substring/code.test.js
+++ problems/0-99/5-longest-palindromic-substring/code.test.js
@@ -1,38 +1,51 @@
-const longestPalindrome = require("./code");
+const {
+  longestPalindrome,
+  longestPalindromeWithDoublePointer,
+} = require("./code");
 
 const str = "babad";
-const subStr = "bab";
+const subStr = "bab or aba";
 test(`Longest palindrome substring of ${str} is ${subStr}`, () => {
   const expected = ['bab', 'aba'].includes(longestPalindrome(str));
+  const expected2 = ['bab', 'aba'].includes(longestPalindromeWithDoublePointer(str));
   expect(expected).toBe(true);
+  expect(expected2).toBe(true);
 });
 
 const str1 = "cbbd";
 const subStr1 = "bb";
 test(`Longest palindrome substring of ${str1} is ${subStr1}`, () => {
   expect(longestPalindrome(str1)).toBe(subStr1);
+  expect(longestPalindromeWithDoublePointer(str1)).toBe(subStr1);
 });
 
 const str2 = "a";
 const subStr2 = "a";
 test(`Longest palindrome substring of ${str2} is ${subStr2}`, () => {
   expect(longestPalindrome(str2)).toBe(subStr2);
+  expect(longestPalindromeWithDoublePointer(str2)).toBe(subStr2);
 });
 
 const str3 = "ac";
-const subStr3 = "a";
+const subStr3 = "a or c";
 test(`Longest palindrome substring of ${str3} is ${subStr3}`, () => {
-  expect(longestPalindrome(str3)).toBe(subStr3);
+  const expected = ['a', 'c'].includes(longestPalindrome(str3));
+  const expected2 = ['a', 'c'].includes(longestPalindromeWithDoublePointer(str3));
+
+  expect(expected).toBe(true);
+  expect(expected2).toBe(true);
 });
 
 const str4 = "acca";
 const subStr4 = "acca";
 test(`Longest palindrome substring of ${str4} is ${subStr4}`, () => {
   expect(longestPalindrome(str4)).toBe(subStr4);
+  expect(longestPalindromeWithDoublePointer(str4)).toBe(subStr4);
 });
 
 const str5 = "abcba";
 const subStr5 = "abcba";
 test(`Longest palindrome substring of ${str5} is ${subStr5}`, () => {
   expect(longestPalindrome(str5)).toBe(subStr5);
+  expect(longestPalindromeWithDoublePointer(str5)).toBe(subStr5);
 });
