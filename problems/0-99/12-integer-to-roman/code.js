4da974d80b7307b84fba048128eb5a5f7eab2ee0
Spike Leung <l-yanlei@hotmail.com>
2021-12-27 16:47:00 +0800

    feat: 12-integer-to-roman (greedy)

--- problems/0-99/12-integer-to-roman/code.js
+++ problems/0-99/12-integer-to-roman/code.js
@@ -3,54 +3,36 @@
  * @param {number} num
  * @return {string}
  */
-var intToRoman = function(num) {
-  let res = [];
-  const romanMap = {
-    1: "I",
-    2: "II",
-    3: "III",
-    4: "IV",
-    5: "V",
-    6: "VI",
-    7: "VII",
-    8: "VIII",
-    9: "IX",
-    10: "X",
-    20: "XX",
-    30: "XXX",
-    40: "XL",
-    50: "L",
-    60: "LX",
-    70: "LXX",
-    80: "LXXX",
-    90: "XC",
-    100: "C",
-    200: "CC",
-    300: "CCC",
-    400: "CD",
-    500: "D",
-    600: "DC",
-    700: "DCC",
-    800: "DCCC",
-    900: "CM",
-    1000: "M",
-    2000: "MM",
-    3000: "MMM",
-    4000: "MMMM",
-  };
+var intToRoman = function (num) {
+  const valueSymbols = [
+    [1000, "M"],
+    [900, "CM"],
+    [500, "D"],
+    [400, "CD"],
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"],
+  ];
+  const roman = [];
 
-  num % 10 > 0 && (res.push(romanMap[num % 10]));
+  for (const [value, symbol] of valueSymbols) {
+    while (num >= value) {
+      num -= value;
+      roman.push(symbol);
+    }
 
-  let carry = 10;
-
-  while (Number.parseInt(num / carry, 10) > 0) {
-    const number = (Number.parseInt(num / carry, 10)) % 10 * carry;
-    number !== 0 && (res.unshift(romanMap[number]));
-    carry *= 10;
+    if (num === 0) {
+      break;
+    }
   }
 
-  return res.join("");
+  return roman.join("");
 };
-
 
 module.exports = intToRoman;
