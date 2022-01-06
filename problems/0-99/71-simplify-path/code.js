d8963d1c51a93f4d0e5a5fa9745dec7a2f8807d9
Spike Leung <l-yanlei@hotmail.com>
2022-01-06 20:25:15 +0800

    feat: 71-simplify-path

--- problems/0-99/71-simplify-path/code.js
+++ problems/0-99/71-simplify-path/code.js
@@ -4,52 +4,20 @@
  * @return {string}
  */
 var simplifyPath = function(path) {
-  let index = 1;
-  let pathStack = ["/"];
+  let paths = path.split("/");
+  const mock = [];
 
-  while (index < path.length) {
-    // 跳过重复的 /
-    while (index < path.length && path[index] === "/") {
-      index++;
-    }
-
-    // 获取路径
-    const start = index;
-    while (index < path.length && path[index] !== "/") {
-      index++;
-    }
-
-    if (start >= path.length) {
-      break;
-    }
-
-    const pathName = path.slice(start, index);
-    // 路径就是当前路径，直接跳过
-    if (pathName !== ".") {
-      // 如果是 .. 并且有父级目录，则弹出当前目录
-      if (pathName === "..") {
-        if (pathStack.length >= 3) {
-          // 分隔符
-          pathStack.pop();
-          // 路径
-          pathStack.pop();
-        }
-      } else {
-        // 有路径则加入路径
-        pathStack.push(pathName);
-        pathStack.push("/");
+  for (let name of paths) {
+    if (name === "..") {
+      if (mock.length > 0) {
+        mock.pop();
       }
+    } else if (name && name !== ".") {
+      mock.push(name);
     }
-
-    index++;
   }
 
-  // 移出最后的 /
-  if (pathStack.length > 1 && pathStack[pathStack.length - 1] === "/") {
-    pathStack.pop();
-  }
-
-  return pathStack.join("");
+  return "/" + mock.join("/");
 };
 
 module.exports = simplifyPath;
