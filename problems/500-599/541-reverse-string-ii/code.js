5ae7397e86baf6ab889d9f06f4132c5137aa04c9
Spike <l-yanlei@hotmail.com>
2022-04-03 17:11:32 +0800

    refactor: update 541

--- problems/500-599/541-reverse-string-ii/code.js
+++ problems/500-599/541-reverse-string-ii/code.js
@@ -8,23 +8,8 @@ var reverseStr = function (s, k) {
   const n = s.length;
   const sArr = [...s];
 
-  if (n < k) {
-    reverse(sArr, 0, n - 1);
-    return sArr.join("");
-  }
-
-  if (n < 2 * k) {
-    reverse(sArr, 0, k - 1);
-    return sArr.join("");
-  }
-
-  let l = 0, r = 2 * k;
-  for (; r < n; r += 2 * k, l += 2 * k) {
-    reverse(sArr, l, l + k - 1);
-  }
-
-  if (l < n) {
-    reverse(sArr, l, Math.min(l + k - 1, n - 1));
+  for (let i = 0; i < n; i += 2 * k) {
+    reverse(sArr, i, Math.min(i + k, n) - 1);
   }
 
   return sArr.join("");
