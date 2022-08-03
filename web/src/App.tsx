ff2eb61434dc29a7a1505a28948315a432f98346
Spike <l-yanlei@hotmail.com>
2022-08-04 02:57:51 +0800

    feat: add syntax highlight

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,31 @@
 import ReactMarkdown from "react-markdown"
 import Markdown from "solution-parser/data/173-binary-search-tree-iterator.md?raw"
-
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 
 function App() {
-  return <ReactMarkdown children={Markdown} />
+  return (
+    <ReactMarkdown
+      className="p-20"
+      children={Markdown}
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "")
+          return !inline && match ? (
+            <SyntaxHighlighter
+              children={String(children).replace(/\n$/, "")}
+              language={match[1]}
+              PreTag="div"
+              {...props}
+            />
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          )
+        },
+      }}
+    />
+  )
 }
 
 export default App
