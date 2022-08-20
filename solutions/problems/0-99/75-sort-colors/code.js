696c82f86034b20c856fa1932b6774815bc934df
Spike <l-yanlei@hotmail.com>
2022-08-21 02:57:20 +0800

    refactor: update 75

--- solutions/problems/0-99/75-sort-colors/code.js
+++ solutions/problems/0-99/75-sort-colors/code.js
@@ -4,33 +4,32 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-  const n = nums.length;
-  let p0 = 0;
-  let p1 = 0;
+  const n = nums.length
+  // all in [0, p0) equals to 0
+  // all in [p0, i) equals to 1
+  // all in (p2, n - 1] equals to 2
+  let p0 = 0
+  let p2 = n - 1
+  let i = 0
 
-  for (let i = 0;  i < n; i++) {
-    if (nums[i] === 1) {
-      swap(nums, p1, i);
-      p1++;
-    } else if (nums[i] === 0) {
-      swap(nums, p0, i);
-
-      if (p0 < p1) {
-        swap(nums, p1, i);
-      }
-
-      p0++;
-      p1++;
+  while (i <= p2) {
+    if (nums[i] === 0) {
+      swap(nums, p0++, i)
+      i++
+    } else if (nums[i] === 1) {
+      i++
+    } else if (nums[i] === 2) {
+      swap(nums, p2--, i)
     }
   }
 
   return nums;
 };
 
-function swap(nums, a, b) {
-  const t = nums[a];
-  nums[a] = nums[b];
-  nums[b] = t;
+function swap(nums, i, j) {
+  const temp = nums[i];
+  nums[i] = nums[j];
+  nums[j] = temp;
 }
 
 module.exports = sortColors;
